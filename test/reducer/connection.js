e5b7186350f1fdbddeafd36a2b42f50ee019152e
Duknam Yoo <yoo2001818@gmail.com>
2015-12-03 21:42:02 +0900

    Refactor test format

--- test/reducer/connection.js
+++ test/reducer/connection.js
@@ -15,213 +15,213 @@ const testConnection2 = {
   name: 'Guest'
 };
 
-describe('reducers', () => {
-  describe('connection', () => {
-    let store;
-    beforeEach('configure store', () => {
-      store = createStore(connectionReducer);
-    });
+describe('connectionReducer', () => {
+  let store;
+  beforeEach('configure store', () => {
+    store = createStore(connectionReducer);
+  });
 
-    it('INIT should init with valid fields', () => {
+  describe('INIT', () => {
+    it('should init with valid fields', () => {
       // We expect createStore already dispatched INIT action.
       expect(store.getState()).toEqual({
         self: null,
         list: {}
       });
     });
+  });
 
-    describe('CONNECT', () => {
-      beforeEach('connect user', () => {
-        store.dispatch(connect(testConnection));
-      });
+  describe('CONNECT', () => {
+    beforeEach('connect user', () => {
+      store.dispatch(connect(testConnection));
+    });
 
-      it('should add connection to the list', () => {
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {
-            1: testConnection
-          }
-        });
-      });
-
-      it('should handle multiple connections', () => {
-        store.dispatch(connect(testConnection2));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {
-            1: testConnection,
-            2: testConnection2
-          }
-        });
-      });
-
-      it('should throw an error if duplicated', () => {
-        expect(() => {
-          store.dispatch(connect(testConnection));
-        }).toThrow();
-      });
-
-      it('should ignore errored actions', () => {
-        store.dispatch(Object.assign({}, connect(testConnection2), {
-          error: true
-        }));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {
-            1: testConnection
-          }
-        });
+    it('should add connection to the list', () => {
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {
+          1: testConnection
+        }
       });
     });
 
-    describe('DISCONNECT', () => {
-      beforeEach('connect user', () => {
-        store.dispatch(connect(testConnection));
-      });
-
-      // Alternatively, connection 'removed' flag can be set.
-      // However, I'll change this if it is required.
-      // Until then, this is default behavior.
-      it('should remove connection from the list', () => {
-        store.dispatch(disconnect(testConnection));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {}
-        });
-      });
-
-      it('should handle multiple connections', () => {
-        store.dispatch(connect(testConnection2));
-
-        store.dispatch(disconnect(testConnection));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {
-            2: testConnection2
-          }
-        });
-
-        store.dispatch(disconnect(testConnection2));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {}
-        });
-      });
-
-      it('should throw an error if not exists', () => {
-        store.dispatch(disconnect(testConnection));
-        expect(() => {
-          store.dispatch(disconnect(testConnection));
-        }).toThrow();
-      });
-
-      it('should ignore errored actions', () => {
-        store.dispatch(Object.assign({}, disconnect(testConnection), {
-          error: true
-        }));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {
-            1: testConnection
-          }
-        });
+    it('should handle multiple connections', () => {
+      store.dispatch(connect(testConnection2));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {
+          1: testConnection,
+          2: testConnection2
+        }
       });
     });
 
-    describe('UPDATE', () => {
-      beforeEach('connect user', () => {
+    it('should throw an error if duplicated', () => {
+      expect(() => {
         store.dispatch(connect(testConnection));
-      });
+      }).toThrow();
+    });
 
-      it('should update connection', () => {
-        store.dispatch(update({
-          id: 1,
-          name: 'So Awesome',
-          // Test arbitrary fields too
-          coolnessModifier: 0.2
-        }));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {
-            1: {
-              id: 1,
-              name: 'So Awesome',
-              coolnessModifier: 0.2
-            }
-          }
-        });
+    it('should ignore errored actions', () => {
+      store.dispatch(Object.assign({}, connect(testConnection2), {
+        error: true
+      }));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {
+          1: testConnection
+        }
       });
+    });
+  });
 
-      it('should throw an error if not exists', () => {
-        store.dispatch(disconnect(testConnection));
-        expect(() => {
-          store.dispatch(update(testConnection));
-        }).toThrow();
-      });
+  describe('DISCONNECT', () => {
+    beforeEach('connect user', () => {
+      store.dispatch(connect(testConnection));
+    });
 
-      it('should ignore errored actions', () => {
-        store.dispatch(Object.assign({}, update({
-          id: 1,
-          name: 'Hello'
-        }), {
-          error: true
-        }));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {
-            1: testConnection
-          }
-        });
+    // Alternatively, connection 'removed' flag can be set.
+    // However, I'll change this if it is required.
+    // Until then, this is default behavior.
+    it('should remove connection from the list', () => {
+      store.dispatch(disconnect(testConnection));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {}
       });
     });
 
-    describe('HANDSHAKE', () => {
-      it('should set state', () => {
-        store.dispatch(handshake({
-          connection: {
-            self: 1,
-            list: {
-              1: testConnection
-            }
+    it('should handle multiple connections', () => {
+      store.dispatch(connect(testConnection2));
+
+      store.dispatch(disconnect(testConnection));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {
+          2: testConnection2
+        }
+      });
+
+      store.dispatch(disconnect(testConnection2));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {}
+      });
+    });
+
+    it('should throw an error if not exists', () => {
+      store.dispatch(disconnect(testConnection));
+      expect(() => {
+        store.dispatch(disconnect(testConnection));
+      }).toThrow();
+    });
+
+    it('should ignore errored actions', () => {
+      store.dispatch(Object.assign({}, disconnect(testConnection), {
+        error: true
+      }));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {
+          1: testConnection
+        }
+      });
+    });
+  });
+
+  describe('UPDATE', () => {
+    beforeEach('connect user', () => {
+      store.dispatch(connect(testConnection));
+    });
+
+    it('should update connection', () => {
+      store.dispatch(update({
+        id: 1,
+        name: 'So Awesome',
+        // Test arbitrary fields too
+        coolnessModifier: 0.2
+      }));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {
+          1: {
+            id: 1,
+            name: 'So Awesome',
+            coolnessModifier: 0.2
           }
-        }));
-        expect(store.getState()).toEqual({
+        }
+      });
+    });
+
+    it('should throw an error if not exists', () => {
+      store.dispatch(disconnect(testConnection));
+      expect(() => {
+        store.dispatch(update(testConnection));
+      }).toThrow();
+    });
+
+    it('should ignore errored actions', () => {
+      store.dispatch(Object.assign({}, update({
+        id: 1,
+        name: 'Hello'
+      }), {
+        error: true
+      }));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {
+          1: testConnection
+        }
+      });
+    });
+  });
+
+  describe('HANDSHAKE', () => {
+    it('should set state', () => {
+      store.dispatch(handshake({
+        connection: {
           self: 1,
           list: {
             1: testConnection
           }
-        });
+        }
+      }));
+      expect(store.getState()).toEqual({
+        self: 1,
+        list: {
+          1: testConnection
+        }
       });
+    });
 
-      it('should ignore previous state', () => {
-        store.dispatch(connect(testConnection2));
-        store.dispatch(handshake({
-          connection: {
-            self: 1,
-            list: {
-              1: testConnection
-            }
-          }
-        }));
-        expect(store.getState()).toEqual({
+    it('should ignore previous state', () => {
+      store.dispatch(connect(testConnection2));
+      store.dispatch(handshake({
+        connection: {
           self: 1,
           list: {
             1: testConnection
           }
-        });
+        }
+      }));
+      expect(store.getState()).toEqual({
+        self: 1,
+        list: {
+          1: testConnection
+        }
       });
+    });
 
-      it('should ignore errored actions', () => {
-        store.dispatch(Object.assign({}, handshake({
-          code: 501,
-          reason: 'Test'
-        }), {
-          error: true
-        }));
-        expect(store.getState()).toEqual({
-          self: null,
-          list: {}
-        });
+    it('should ignore errored actions', () => {
+      store.dispatch(Object.assign({}, handshake({
+        code: 501,
+        reason: 'Test'
+      }), {
+        error: true
+      }));
+      expect(store.getState()).toEqual({
+        self: null,
+        list: {}
       });
     });
   });
--- test/reducer/subChat.js
+++ test/reducer/subChat.js
@@ -11,89 +11,89 @@ const testUser = {
 const testMessage = {
   type: 'normal',
   message: 'Hello, world!',
-  connection: 1 
+  connection: 1
 };
 
-describe('reducers', () => {
-  describe('subChat', () => {
-    let store;
-    beforeEach('configure store', () => {
-      store = createStore(subChatReducer);
-    });
+describe('subChatReducer', () => {
+  let store;
+  beforeEach('configure store', () => {
+    store = createStore(subChatReducer);
+  });
 
-    it('INIT should init with valid fields', () => {
+  describe('INIT', () => {
+    it('should init with valid fields', () => {
       // We expect createStore already dispatched INIT action.
       expect(store.getState()).toEqual({
         limit: 100,
         messages: []
       });
     });
+  });
 
-    describe('CHAT', () => {
-      it('should append incoming message', () => {
-        // in subChat router, the scope, 'lobby' isn't checked actually.
-        // However, this is used to route the action by chat router.
-        store.dispatch(chat('lobby', testUser, 'Hello, world!'));
-        expect(store.getState()).toEqual({
-          limit: 100,
-          messages: [
-            testMessage
-          ]
-        });
+  describe('CHAT', () => {
+    it('should append incoming message', () => {
+      // in subChat router, the scope, 'lobby' isn't checked actually.
+      // However, this is used to route the action by chat router.
+      store.dispatch(chat('lobby', testUser, 'Hello, world!'));
+      expect(store.getState()).toEqual({
+        limit: 100,
+        messages: [
+          testMessage
+        ]
       });
+    });
 
-      it('should automatically clip incoming messages', () => {
-        // Put 100 messages at once.
-        let messages = [];
-        for (let i = 0; i < 100; ++i) {
-          store.dispatch(chat('lobby', testUser, 'Hello, world!'));
-          messages.push(testMessage);
-          expect(store.getState()).toEqual({
-            limit: 100,
-            messages
-          });
-        }
-        // Now, try this again and check if it's clipped.
-        messages.shift();
-        messages.push(Object.assign({}, testMessage, {
-          message: 'Stop spamming!'
-        }));
-        store.dispatch(chat('lobby', testUser, 'Stop spamming!'));
+    it('should automatically clip incoming messages', () => {
+      // Put 100 messages at once.
+      let messages = [];
+      for (let i = 0; i < 100; ++i) {
+        store.dispatch(chat('lobby', testUser, 'Hello, world!'));
+        messages.push(testMessage);
         expect(store.getState()).toEqual({
           limit: 100,
           messages
         });
+      }
+      // Now, try this again and check if it's clipped.
+      messages.shift();
+      messages.push(Object.assign({}, testMessage, {
+        message: 'Stop spamming!'
+      }));
+      store.dispatch(chat('lobby', testUser, 'Stop spamming!'));
+      expect(store.getState()).toEqual({
+        limit: 100,
+        messages
       });
     });
-
-    describe('CLEAR_HISTORY', () => {
-      it('should clear history', () => {
-        // We need to put dummy data into the store before testing this.
-        store.dispatch(chat('lobby', testUser, 'Hello, world!'));
-        expect(store.getState()).toEqual({
-          limit: 100,
-          messages: [
-            testMessage
-          ]
-        });
-        // Now, test for real.
-        store.dispatch(clearHistory('lobby'));
-        expect(store.getState()).toEqual({
-          limit: 100,
-          messages: []
-        });
-      });
-    });
-
-    describe('SET_LIMIT', () => {
-      it('should set limit', () => {
-        store.dispatch(setLimit('lobby', 200));
-        expect(store.getState()).toEqual({
-          limit: 200,
-          messages: []
-        });
-      });
-    });
-
   });
+
+  describe('CLEAR_HISTORY', () => {
+    it('should clear history', () => {
+      // We need to put dummy data into the store before testing this.
+      store.dispatch(chat('lobby', testUser, 'Hello, world!'));
+      expect(store.getState()).toEqual({
+        limit: 100,
+        messages: [
+          testMessage
+        ]
+      });
+      // Now, test for real.
+      store.dispatch(clearHistory('lobby'));
+      expect(store.getState()).toEqual({
+        limit: 100,
+        messages: []
+      });
+    });
+  });
+
+  describe('SET_LIMIT', () => {
+    it('should set limit', () => {
+      store.dispatch(setLimit('lobby', 200));
+      expect(store.getState()).toEqual({
+        limit: 200,
+        messages: []
+      });
+    });
+  });
+
 });
