8e06624e41c71b4a602b5131e2cb212cf8761acb
Duknam Yoo <yoo2001818@gmail.com>
2015-12-03 12:26:12 +0900

    Reformat subChat reducer test

--- test/reducer/subChat.js
+++ test/reducer/subChat.js
@@ -21,7 +21,7 @@ describe('reducers', () => {
       store = createStore(subChatReducer);
     });
 
-    it('should init with valid fields', () => {
+    it('INIT should init with valid fields', () => {
       // We expect createStore already dispatched INIT action.
       expect(store.getState()).toEqual({
         limit: 100,
@@ -29,63 +29,69 @@ describe('reducers', () => {
       });
     });
 
-    it('should process CHAT action', () => {
-      // in subChat router, the scope, 'lobby' isn't checked actually.
-      // However, this is used to route the action by chat router.
-      store.dispatch(chat('lobby', testUser, 'Hello, world!'));
-      expect(store.getState()).toEqual({
-        limit: 100,
-        messages: [
-          testMessage
-        ]
-      });
-    });
-
-    it('should automatically clip incoming messages', () => {
-      // Put 100 messages at once.
-      let messages = [];
-      for (let i = 0; i < 100; ++i) {
+    describe('CHAT', () => {
+      it('should append incoming message', () => {
+        // in subChat router, the scope, 'lobby' isn't checked actually.
+        // However, this is used to route the action by chat router.
         store.dispatch(chat('lobby', testUser, 'Hello, world!'));
-        messages.push(testMessage);
+        expect(store.getState()).toEqual({
+          limit: 100,
+          messages: [
+            testMessage
+          ]
+        });
+      });
+
+      it('should automatically clip incoming messages', () => {
+        // Put 100 messages at once.
+        let messages = [];
+        for (let i = 0; i < 100; ++i) {
+          store.dispatch(chat('lobby', testUser, 'Hello, world!'));
+          messages.push(testMessage);
+          expect(store.getState()).toEqual({
+            limit: 100,
+            messages
+          });
+        }
+        // Now, try this again and check if it's clipped.
+        messages.shift();
+        messages.push(Object.assign({}, testMessage, {
+          message: 'Stop spamming!'
+        }));
+        store.dispatch(chat('lobby', testUser, 'Stop spamming!'));
         expect(store.getState()).toEqual({
           limit: 100,
           messages
         });
-      }
-      // Now, try this again and check if it's clipped.
-      messages.shift();
-      messages.push(Object.assign({}, testMessage, {
-        message: 'Stop spamming!'
-      }));
-      store.dispatch(chat('lobby', testUser, 'Stop spamming!'));
-      expect(store.getState()).toEqual({
-        limit: 100,
-        messages
       });
     });
 
-    it('should process CLEAR_HISTORY action', () => {
-      // We need to put dummy data into the store before testing this.
-      store.dispatch(chat('lobby', testUser, 'Hello, world!'));
-      expect(store.getState()).toEqual({
-        limit: 100,
-        messages: [
-          testMessage
-        ]
-      });
-      // Now, test for real.
-      store.dispatch(clearHistory('lobby'));
-      expect(store.getState()).toEqual({
-        limit: 100,
-        messages: []
+    describe('CLEAR_HISTORY', () => {
+      it('should clear history', () => {
+        // We need to put dummy data into the store before testing this.
+        store.dispatch(chat('lobby', testUser, 'Hello, world!'));
+        expect(store.getState()).toEqual({
+          limit: 100,
+          messages: [
+            testMessage
+          ]
+        });
+        // Now, test for real.
+        store.dispatch(clearHistory('lobby'));
+        expect(store.getState()).toEqual({
+          limit: 100,
+          messages: []
+        });
       });
     });
 
-    it('should process SET_LIMIT action', () => {
-      store.dispatch(setLimit('lobby', 200));
-      expect(store.getState()).toEqual({
-        limit: 200,
-        messages: []
+    describe('SET_LIMIT', () => {
+      it('should set limit', () => {
+        store.dispatch(setLimit('lobby', 200));
+        expect(store.getState()).toEqual({
+          limit: 200,
+          messages: []
+        });
       });
     });
 
