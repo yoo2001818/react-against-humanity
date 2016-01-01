1ddb3a23fe02c3101d99f9444c270dfc6d67bad7
Duknam Yoo <yoo2001818@gmail.com>
2016-01-01 21:17:08 +0900

    Implement scroll lock
    Signed-off-by: Duknam Yoo <yoo2001818@gmail.com>

--- src/component/messageList.js
+++ src/component/messageList.js
@@ -1,7 +1,23 @@
 import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
 import MessageItem from './messageItem';
 
 export default class MessageList extends Component {
+  componentDidMount() {
+    let node = findDOMNode(this);
+    node.scrollTop = node.scrollHeight;
+  }
+  componentWillUpdate() {
+    let node = findDOMNode(this);
+    this.shouldScrollBottom =
+      node.scrollTop + node.offsetHeight === node.scrollHeight;
+  }
+  componentDidUpdate() {
+    if (this.shouldScrollBottom) {
+      let node = findDOMNode(this);
+      node.scrollTop = node.scrollHeight;
+    }
+  }
   render() {
     const { messages } = this.props;
     return (
