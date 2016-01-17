02d24cae1f2b7fd930c16e8bc44eb885c563265d
Duknam Yoo <yoo2001818@gmail.com>
2016-01-17 19:19:15 +0900

    Improve router logger

--- src/router/middleware/logger.js
+++ src/router/middleware/logger.js
@@ -1,6 +1,24 @@
 // Just a logger.
 
+/* eslint-disable no-console */
+
 export default function logger(req, res, next) {
-  console.log(req.action, req.connection); // eslint-disable-line no-console
+  let type = (req.action && req.action.type) || '@@invalid';
+  let time = new Date().toTimeString();
+  let target = 'dispatch';
+  if (req.cause === 'poll') target = 'poll/' + req.connection;
+  let targetColor = target === 'dispatch' ? 'magenta' : 'yellow';
+  // Ignore dispatch actions, as it is not really necessary to process in here.
+  if (target === 'dispatch') return next();
+  if (__SERVER__) {
+    const colors = require('colors/safe');
+    console.log(colors[targetColor](target) + ' @ ' +
+      colors.blue(type) + ' ' + time);
+    console.log('  payload:', req.action.payload);
+  } else {
+    console.log('%c%s%c @ %c%s%c %s', `color: #3085D5`, target, '',
+      'font-weight: bold; color: #39A63A', type, '', time);
+    console.log('%c  payload: %o', 'color: #000', req.action.payload);
+  }
   next();
 }
